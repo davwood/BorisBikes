87234982f056109ad73410d0cb05424718083ab9
davewood <davwood_1@yahoo.com.au>
2013-09-05 13:12:27 +0100

    Jay's updates

--- lib/person.rb
+++ lib/person.rb
@@ -1,28 +1,26 @@
-# require_relative '../lib/bike.rb'
-# require_relative '../lib/bike_station.rb'
-
 class Person
 
-	def initialize
+	def initialize(name)
 		@bike = nil
 	end
 
 	def rent_bike(fromstation)
-		@bike = fromstation.checkout 
+		@bike = fromstation.checkout
+
 	end
 
 	def return_bike(tostation)
 		tostation.checkin(@bike)
-		@bike = nil
+
 	end
 
 	def has_bike?
 		!@bike.nil?
 	end
 
-	
 	def broken_bike_alert(bike)
 		bike.break!
+
 	end
 
 	def rentedbike
@@ -31,4 +29,3 @@ class Person
 
 
 end
-
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,37 +1,52 @@
-require_relative '../lib/person'
+require'../lib/person'
+#require_relative wasn't working for some reason
 
 
 describe Person do
-	
-	let(:aperson) {Person.new}
+
+	let(:aperson) {Person.new('jay')}
 	let(:fromstation) { double(:fromstation)}
 	let(:tostation) { double(:tostation)}
 	let(:bike) {double(:bike)}
-		
+
 
 	it 'can check out a bike from a station' do
 		expect(fromstation).to receive(:checkout)
-		aperson.takebike(fromstation)
+		aperson.rent_bike(fromstation)
 	end
 
-	it 'can return a bike to a different station' do
-		expect(tostation).to receive(:checkin)
-		aperson.returnbike(tostation)
+	it 'can return a bike to the station' do
+		# expect(fromstation).to receive(:checkout).and_return(bike)
+		# aperson.rent_bike(fromstation)
+
+		# expect(tostation).to receive(:checkin).with(bike)
+		# aperson.return_bike(tostation) 
+
+		expect(fromstation).to receive(:checkout).and_return(bike)
+		aperson.rent_bike(fromstation)
+
+		expect(tostation).to receive(:checkin).with(bike)
+		aperson.return_bike(tostation)
+
+	end
+
+	it 'does not have a bike after returning it to the station' do
+
 	end
 
 	it 'can alert the bike station that a bike is broken' do
-		expect(bike).to receive (:break!) 
-		aperson.brokenbikealert(bike)
+		expect(bike).to receive(:break!) 
+		aperson.broken_bike_alert(bike)
 	end
 
-	# it 'can return a bike to the same station' do
-
-	# end
-
-	it 'will show that the person has the bike' do
-		
-		
+	it 'can show that they do not have a bike' do
+		expect(aperson).to receive(:has_bike?)
+		expect(aperson.has_bike?).to eq(nil)
 	end
 
+	it 'can show that they have a bike' do
+		expect(aperson).to receive(:has_bike?)
+		expect(aperson.has_bike?).to eq(puts "has bike? #{@has_bike = true}")
+	end
 
 end
